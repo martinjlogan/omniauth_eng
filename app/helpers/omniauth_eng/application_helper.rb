40778fb6ae1e2dba5360e5ed46bfb32f26bdf40f
martinjlogan <martinjlogan@gmail.com>
2013-01-02 21:33:56 -0800

    added helpers for current user

--- app/helpers/omniauth_eng/application_helper.rb
+++ app/helpers/omniauth_eng/application_helper.rb
@@ -1,7 +1,23 @@
 module OmniauthEng
 	module ApplicationHelper
 		def current_user
-			@current_user ||= User.find(session[:user_id]) if session[:user_id]
-		end
+                        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+                end
+
+                def writer_user
+                        if current_user
+                                current_user.is_writer?
+                        else
+                                nil
+                        end
+                end
+
+                def admin_user
+                        if current_user
+                                current_user.is_admin?
+                        else
+                                nil
+                        end
+                end
 	end
 end
